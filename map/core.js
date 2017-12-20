5883bba7f6dad1d46f36e7682204c96c53941995
Michael Schreier <mjschreier1@gmail.com>
2017-12-20 15:05:06 -0700

    completed map exercises

--- map/core.js
+++ map/core.js
@@ -5,20 +5,28 @@ function multiplyBy10 (array) {
 };
 
 function shiftRight (array) {
- // your code here
+  return array.map(function(element, index, arr) {
+    return arr[index - 1] || arr[arr.length - 1];
+  })
 };
 
 function onlyVowels (array) {
- // your code here
+  return array.map(function(string) {
+    return string.replace(/[^aeiou]/ig,"")
+  })
 };
 
 function doubleMatrix (array) {
- // your code here
+  return array.map(function(row) {
+    return row.map(function(value) {
+      return value * 2
+    })
+  })
 };
 
 module.exports = {
   multiplyBy10: multiplyBy10,
-  // shiftRight: shiftRight,
-  // onlyVowels: onlyVowels,
-  // doubleMatrix: doubleMatrix
+  shiftRight: shiftRight,
+  onlyVowels: onlyVowels,
+  doubleMatrix: doubleMatrix
 };
